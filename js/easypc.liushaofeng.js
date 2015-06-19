0fb2d877c26c842f84b7c05c84651872ca5d29ee
liushaofeng <liushaofeng89@qq.com>
2015-06-19 20:16:21 +0800

    更新联系我们页面

---
+++ js/easypc.liushaofeng.js
@@ -0,0 +1,2 @@
+//baidu share script
+window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"5","bdPos":"right","bdTop":"250"},"image":{"viewList":["qzone","tsina","tqq","renren","weixin"],"viewText":"∑÷œÌµΩ£∫","viewSize":"16"}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
